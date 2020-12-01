d579470fb14f7698e1fd453e4b81d84662da34b5
Ahmad Faris <isfawwaz@gmail.com>
2020-12-01 21:10:37 +0700

    Add platform restriction middleware

---
+++ app/Http/Middleware/PlatformRestriction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Developer;
+use Closure;
+use Illuminate\Http\Request;
+
+class PlatformRestriction
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, ...$platforms)
+    {
+        $headerName = 'x-g-access-token';
+        if( !$request->hasHeader($headerName) ) {
+            return abort(417);
+        }
+
+        $accessToken = $request->header( $headerName );
+        $find = Developer::where('dev_public_key', $accessToken)->first();
+        if( !empty($find) ) {
+            $scope = unserialize( $find->scope );
+            $p = $scope['platform'];
+            if( !empty($platforms) ) {
+                if( in_array( $p, $platforms) ) {
+                    return $next($request);
+                }
+            } else {
+                return $next($request);
+            }
+        }
+
+        return abort(417);
+    }
+}
