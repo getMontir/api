5f363de0174e79906721961aa978308d266c5eac
Ahmad Faris <isfawwaz@gmail.com>
2020-12-27 23:24:26 +0700

    Fixing style

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -2,10 +2,10 @@
     <td>
         <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
             <tr>
-                <td class="content-cell" align="left">
+                <td class="content-cell" align="left" width="50%">
                     {{ Illuminate\Mail\Markdown::parse($slot) }}
                 </td>
-                <td class="content-cell" align="right">
+                <td class="content-cell" align="right" width="50%">
                     {{-- SOCIAL MEDIA --}}
                 </td>
             </tr>
